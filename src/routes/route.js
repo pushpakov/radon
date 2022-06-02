c68e0d21ef57a67516df1db7121feffeba8c2758
pushpak <pushpak99311@gmail.com>
2022-06-02 22:05:46 +0530

    Missing number

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const myHelper = require('../util/helper')
 const underscore = require('underscore')
 
+const app = express.Router();
 const router = express.Router();
+
 
 router.get('/test-me', function (req, res) {
     myHelper.printDate()
@@ -36,6 +38,47 @@ router.get('/candidates/:canidatesName', function(req, res){
     res.send('Done')
 })
 
+router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
 
+    ///LOGIC WILL GO HERE 
+    //const totalSum = 6*(6+1)/2
+    let currentSum = 0
+    for (let i =0;i<arr.length;i++){
+        currentSum += arr[i]
+    }
+
+    let lastDigit = arr.pop()
+    const totalSum = lastDigit*(lastDigit+1)/2
+    missingNumber = totalSum-currentSum
+
+    res.send(  { data: missingNumber  }  );
+});
+
+
+router.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let firstDigit= arr[0]
+    let lastDigit= arr.pop()
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+    let missingNumber= consecutiveSum - total
+   
+    res.send(  { data: missingNumber  }  );
+  });
+
+  
+ 
+
+
+module.exports = app;
 module.exports = router;
 // adding this comment for no reason
