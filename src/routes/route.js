4f68b6efd5ebe92cf90cc30577fccfb26916a796
pushpak <pushpak99311@gmail.com>
2022-06-03 22:59:30 +0530

    assignment for get-api is done

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,78 +6,7 @@ const app = express.Router();
 const router = express.Router();
 
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
-});
-
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
-
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
-
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
-
-router.get("/sol1", function (req, res) {
-    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
-    let arr= [1,2,3,5,6,7]
-
-    ///LOGIC WILL GO HERE 
-    //const totalSum = 6*(6+1)/2
-    let currentSum = 0
-    for (let i =0;i<arr.length;i++){
-        currentSum += arr[i]
-    }
-
-    let lastDigit = arr.pop()
-    const totalSum = lastDigit*(lastDigit+1)/2
-    missingNumber = totalSum-currentSum
-
-    res.send(  { data: missingNumber  }  );
-});
-
-
-router.get("/sol2", function (req, res) {
-    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
-    let arr= [33, 34, 35, 37, 38]
-    let len= arr.length
-  
-    let total = 0;
-    for (var i in arr) {
-        total += arr[i];
-    }
-  
-    let firstDigit= arr[0]
-    let lastDigit= arr.pop()
-    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
-    let missingNumber= consecutiveSum - total
-    
-   
-    res.send(  { data: missingNumber  }  );
-  });
-
-  
-  router.get("/movies", function(req, res){
+router.get("/movies", function(req, res){
     let movies = ["Rang de basanti","The shining","Lord of the rings","Batman begins"]
     res.send(  { movies }  );
 });
