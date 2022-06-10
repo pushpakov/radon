01bbab0ba50684c417bcab314080c02efd089213
pushpak <pushpak99311@gmail.com>
2022-06-10 19:02:18 +0530

    updated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,9 @@
-const UserModel= require("../models/userModel")
-
-
-
-
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
-
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+const timeIpRoute = function (req, res){
+  
+    const date = new Date()
+    let dateAndTime = date.getFullYear() + "-" + (date.getMonth()+ 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+    console.log( dateAndTime + " ,", req.ip + " ,", req.route.path);
+    res.send({msg: "Hello middleware"})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+module.exports.timeIpRoute = timeIpRoute
