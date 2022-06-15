086e3bc6feca453f47a9b2019baaaef242e80225
pushpak <pushpak99311@gmail.com>
2022-06-15 13:06:15 +0530

    updated

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,7 +3,7 @@ const userModel = require("../models/userModel");
 
 //- Once, all the apis are working fine, move the authentication related code in a middleware called auth.js
 
-const middleAuth = function (req, res, next) {
+const middleAuth = function (req, res,next) {
     let token = req.headers["x-Auth-token"];
     if (!token) token = req.headers["x-auth-token"];
     
@@ -13,9 +13,6 @@ const middleAuth = function (req, res, next) {
     let decodedToken = jwt.verify(token, "functionup-radon");
     if (!decodedToken)
       return res.send({ status: false, msg: "token is invalid" })
-    //   console.log(decodedToken)
-    //   console.log(Date.now())
-    //   console.log((decodedToken.exp*1000)-Date.now())
 
     next();
   };
